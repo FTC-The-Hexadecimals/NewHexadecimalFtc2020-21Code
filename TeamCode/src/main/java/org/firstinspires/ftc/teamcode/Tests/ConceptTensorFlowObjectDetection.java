57acf459409572bbdf0b976ce5c6dfa07168bb0d
dgorbatov <dgorbatov@hotmail.com>
2021-03-12 13:43:49 -0800

    Removed dissabled concept tenserflow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ConceptTensorFlowObjectDetection.java
@@ -51,8 +51,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
+@TeleOp(name = "Concept: TensorFlow Object Detection")
 public class ConceptTensorFlowObjectDetection extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
